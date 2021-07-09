82191ca5765b192824d83d59ab346893c8d67300
taruntiwaritt <35301186+taruntiwaritt@users.noreply.github.com>
2021-07-09 23:00:09 +0530

    Add middleware content --refactor async.js

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,17 @@
-export default function ({ dispatch }) {
-  return function (next) {
-    return function (action) {
-      //do something with the action. This is the same action what actionCreator returns
-    };
+export default ({ dispatch }) =>
+  (next) =>
+  (action) => {
+    //check to see if an action has a promise on payload property
+
+    //if it doesn't send it to next middleware
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
+
+    //wait for promise to resolve then create new action with resolved data and then dispatch it
+    action.payload.then((response) => {
+      const newAction = { ...action, payload: response };
+      //dispatch sends it through all middlewres again
+      dispatch(newAction);
+    });
   };
-}
