b406b2f73aa5b98b2dae43cfd4065fae50984e95
taruntiwaritt <35301186+taruntiwaritt@users.noreply.github.com>
2021-07-09 22:12:12 +0530

    Added HOC for CommentBox

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
+import { connect, Connect } from "react-redux";
+import * as actions from "actions";
 
-export default () => {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  );
-};
+class App extends Component {
+  renderButton() {
+    return this.props.auth ? (
+      <button onClick={() => this.props.changeAuth(false)}> Sign Out</button>
+    ) : (
+      <button onClick={() => this.props.changeAuth(true)}> Sign In</button>
+    );
+  }
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post Commet</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps, actions)(App);
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -10,6 +10,7 @@ class CommentList extends Component {
   render() {
     return (
       <div>
+        <h4>CommentList</h4>
         <ul>{this.renderComments()}</ul>
       </div>
     );
