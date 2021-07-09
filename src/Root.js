72cc95c89988c62cc2ded19453f7f3bd4f02001f
taruntiwaritt <35301186+taruntiwaritt@users.noreply.github.com>
2021-07-10 00:19:29 +0530

    Add custom middlewares for Redux-promise and stateValidator (#3)
    * added redux tests
    
    * Added api calls and integration tests using moxios
    
    * Added HOC for CommentBox
    
    * Create new middleware async.js
    
    * Add middleware content
    --refactor async.js
    
    * Replace ReduxPromise with custom async
    
    * Add stateValidator middleware

--- src/Root.js
+++ src/Root.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import reducers from "reducers";
-import reduxPromise from "redux-promise";
+import async from "middlewares/async";
+import stateValidator from "middlewares/stateValidator";
 
 export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise)
+    applyMiddleware(stateValidator, async)
   );
   return <Provider store={store}>{children}</Provider>;
 };
